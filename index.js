5010228c9e0ed2fcb88d123f370d775a34d8ad5a
q-dot-jpg <129009985+q-dot-jpg@users.noreply.github.com>
2026-03-13 19:07:37 +0000

    to uncapitalise

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let pound = 0
 btn.addEventListener("click", function() {
     console.log(input.value)
     convert(input.value)
-    mf.innerHTML = `${input.value} Meters = ${feet} feet | ${input.value} feet = ${meter} meters`
-    lg.innerHTML = `${input.value} Litres = ${gallon} gallons | ${input.value} gallons = ${liter} litres`
+    mf.innerHTML = `${input.value} meters = ${feet} feet | ${input.value} feet = ${meter} meters`
+    lg.innerHTML = `${input.value} litres = ${gallon} gallons | ${input.value} gallons = ${liter} litres`
     kp.innerHTML = `${input.value} kilos = ${pound} pounds | ${input.value} pounds = ${kilo} kilos`
 })
 
